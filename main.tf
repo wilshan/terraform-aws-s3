906a9ee79898d3cab652b303f0fcdcd54d9951c4
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-16 21:47:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,5 +11,29 @@ resource "aws_s3_bucket" "bucket_name" {
    versioning {
     enabled = "var.versioning_enabled"
   }
+   dynamic "lifecycle_rule" {
+    for_each = var.lifecycle_rules
+    content {
+      enabled                                = lifecycle_rule.value.enabled
+      prefix                                 = lifecycle_rule.value.prefix
+      tags                                   = lifecycle_rule.value.tags
+      abort_incomplete_multipart_upload_days = lifecycle_rule.value.abort_incomplete_multipart_upload_days
+      
+noncurrent_version_transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+      
+      
+      
+      #dynamic "transition" {
+     #   for_each = lifecycle_rule.value.enable_standard_ia_transition ? [1] : []
 
+      #  content {
+       #   days          = lifecycle_rule.value.standard_transition_days
+          #storage_class = "STANDARD_IA"
+        #}
+    # noncurrent_version_expiration {
+   #     days = lifecycle_rule.value.noncurrent_version_expiration_days
+   #}
 }
