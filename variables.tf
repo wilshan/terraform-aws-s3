b02f93fddd31dcc2fa231d9a78cf081ed2a401f3
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-16 23:21:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
+
+variable "acl" {
+  description = "details of Access control list"
+  type        = string
+  default     = "private"
+}
+
 variable "versioning_enabled" {
   description = "A state of versioning"
   type        = bool
@@ -5,46 +12,20 @@ variable "versioning_enabled" {
 }
 
 variable "lifecycle_rules" {
+  description = "A list of lifecycle rules"
   type = list(object({
     prefix  = string
     enabled = bool
     tags    = map(string)
 
-    enable_glacier_transition        = bool
-    enable_deeparchive_transition    = bool
-    enable_standard_ia_transition    = bool
-    enable_current_object_expiration = bool
+    enable_standard_ia_transition = bool
+   
 
-    abort_incomplete_multipart_upload_days         = number
-    noncurrent_version_glacier_transition_days     = number
-    noncurrent_version_deeparchive_transition_days = number
-    noncurrent_version_expiration_days             = number
+    abort_incomplete_multipart_upload_days = number
+    noncurrent_version_expiration_days = number
 
     standard_transition_days    = number
-    glacier_transition_days     = number
-    deeparchive_transition_days = number
     expiration_days             = number
   }))
-  default = [{
-    enabled = false
-    prefix  = ""
-    tags    = {}
-
-    enable_glacier_transition        = true
-    enable_deeparchive_transition    = false
-    enable_standard_ia_transition    = false
-    enable_current_object_expiration = true
-
-    abort_incomplete_multipart_upload_days         = 90
-    noncurrent_version_glacier_transition_days     = 30
-    noncurrent_version_deeparchive_transition_days = 60
-    noncurrent_version_expiration_days             = 90
-
-    standard_transition_days    = 30
-    glacier_transition_days     = 60
-    deeparchive_transition_days = 90
-    expiration_days             = 90
-  }]
-  description = "A list of lifecycle rules"
 }
 
