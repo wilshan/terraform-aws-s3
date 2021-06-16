12abaf03bb777ae1591f56870f668fe895a044ae
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-16 21:29:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+# Bucket creation to keep source data
 
+resource "aws_s3_bucket" "bucket_name" {
+  bucket = "var.bucketname"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+   versioning {
+    enabled = "var.versioning_enabled"
+  }
+
+}
