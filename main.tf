6a94d7ade196f4cb82e08dfca8665188957701a9
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-17 15:30:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,16 @@ resource "aws_s3_bucket" "bucket_source_data" {
   tags = {
     environment = "test"
   }
+  
  versioning {
     enabled = var.versioning_enabled
   }
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = var.kms_arn
-        sse_algorithm     = "aws:kms"
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = var.kms_master_key_id
+        sse_algorithm     = var.sse_algorithm
       }
     }
   }
