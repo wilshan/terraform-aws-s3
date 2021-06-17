93fdeb98f06539d897129ff1d6752fb72feb1071
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-17 14:55:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,45 +5,4 @@ resource "aws_s3_bucket" "bucket_source_data" {
   acl    = "var.acl"
   tags   = "var.tags"
   versioning = "var.versioning_enabled"
-   server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.key.key_id
-        sse_algorithm     = "aws:kms"
-      }
-    }
   }
-   lifecycle_rule {
-    for_each = var.lifecycle_rules
-    content {
-      enabled                                = lifecycle_rule.value.enabled
-      prefix                                 = lifecycle_rule.value.prefix
-      tags                                   = lifecycle_rule.value.tags
-      abort_incomplete_multipart_upload_days = lifecycle_rule.value.abort_incomplete_multipart_upload_days 
-    }
-    noncurrent_version_transition {
-        for_each = lifecycle_rule.value.enable_standard_ia_transition ? [1] : []
-        content {
-          days          = lifecycle_rule.value.noncurrent_version_standard_ia_transition_days
-          storage_class = "STANDARD_IA"
-        }
-      }
-     
-    transition {
-        for_each = lifecycle_rule.value.enable_standard_ia_transition ? [1] : []
-
-        content {
-          days          = lifecycle_rule.value.standard_transition_days
-          storage_class = "STANDARD_IA"
-        }
-      }
-     
-   expiration {
-        for_each = lifecycle_rule.value.enable_current_object_expiration ? [1] : []
-
-        content {
-          days = lifecycle_rule.value.expiration_days
-        }
-      }
-   }
-}
