359ecac6a67426a0e1c4ed83050d4b1518c0d4a8
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-17 16:50:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,4 +19,30 @@ resource "aws_s3_bucket" "bucket_source_data" {
       }
     }
   }
+  
+  lifecycle_rule {
+    id      = var.lifecycle_rule_id
+    enabled = var.lifecycle_rule_enabled
+
+    prefix = "*"
+
+    tags = {
+      rule      = "log"
+      autoclean = "true"
+    }
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA" # or "ONEZONE_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 90
+    }
+  }
 }
