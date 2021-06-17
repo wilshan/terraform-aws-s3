7d7a0f20caadbe94cf7c9e940e853a910918f956
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-17 21:20:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,11 +59,19 @@ resource "aws_s3_bucket" "bucket_source_data" {
       }
     }
   }
-  resource "aws_s3_bucket_public_access_block" "bucket_source_data" {
-  bucket                  = aws_s3_bucket.bucket_source_data.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  
+ resource "aws_s3_bucket_public_access_block" "s3Public" {
+bucket = "${aws_s3_bucket.bucket_source_data.id}"
+block_public_acls = true
+block_public_policy = true
+ignore_public_acls      = true
+restrict_public_buckets = true
 }
+ # resource "aws_s3_bucket_public_access_block" "bucket_source_data" {
+# bucket                  = aws_s3_bucket.bucket_source_data.id
+ # block_public_acls       = true
+ # block_public_policy     = true
+  #ignore_public_acls      = true
+  #restrict_public_buckets = true
+#}
 }
