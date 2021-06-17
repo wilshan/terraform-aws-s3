6dcce21632572142ec0f0a0810b58e902588151f
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-17 15:03:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_s3_bucket" "bucket_source_data" {
   tags = {
     environment = "test"
   }
-}
-  {
-  versioning = var.versioning_enabled
+ versioning {
+    enabled = var.versioning_enabled
   }
