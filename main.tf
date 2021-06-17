250ae068e7bf37e904a5e8edcd017e14a10454cc
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-17 15:16:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "bucket_source_data" {
   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
-        kms_master_key_id = var.kms_id
+        kms_master_key_id = var.kms_arn
         sse_algorithm     = "aws:kms"
       }
     }
