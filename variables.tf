ca280790576cb2d1bb5e34305c0b15b59e7538aa
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-17 15:30:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,5 +28,14 @@ variable "versioning_enabled" {
   default     = true
 }
 
-variable "kms_arn" {}
+variable "kms_master_key_id" {
+    type        = string
+    description = "(optional) The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms."
+    default     = ""
+}
 
+variable "sse_algorithm" {
+    type        = string
+    description = "(required) The server-side encryption algorithm to use. Valid values are AES256 and aws:kms"
+    default     = ""
+}
